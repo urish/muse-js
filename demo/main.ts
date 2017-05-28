71f0e26b97ef143bc12849ab8728b4dbc94090f6
Uri Shaked <uri@salsa4fun.co.il>
2017-05-28 00:20:50 -0400

    feat: plot EEG readings

--- demo/main.ts
+++ demo/main.ts
@@ -1,13 +1,37 @@
-import { MuseClient } from './../lib/muse';
+import { MuseClient, EEGReading } from './../lib/muse';
 
 (window as any).connect = async () => {
+    let canvases = Array.from(document.querySelectorAll('canvas'));
+    let canvasCtx = canvases.map(canvas => canvas.getContext('2d'));
+
+    function plot(reading: EEGReading) {
+        const canvas = canvases[reading.electrode];
+        const context = canvasCtx[reading.electrode];
+        if (!context) {
+            return;
+        }
+        const width = canvas.width / 12.0;
+        const height = canvas.height / 2.0;
+        context.fillStyle = 'green';
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (let i = 0; i < reading.samples.length; i++) {
+            const sample = reading.samples[i] / 50.;
+            if (sample > 0) {
+                context.fillRect(i * 25, height - sample, width, sample);
+            } else {
+                context.fillRect(i * 25, height, width, -sample);
+            }
+        }
+    }
+
     let client = new MuseClient();
     try {
         await client.connect();
         console.log('Connected!');
         await client.start();
         client.eegReadings.subscribe(reading => {
-            console.log('eeg reading', reading);
+            plot(reading);
         });
     } catch (err) {
         console.error('Connection failed', err);
